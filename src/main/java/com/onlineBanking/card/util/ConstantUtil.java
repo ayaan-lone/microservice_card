8207ab09260dc3c81cf960ad4a70c8d7d63c8b43
Himanshu Singh Dhapola <himanshu.dhapola@xebia.com>
2024-07-15 17:50:12 +0530

    Refactored code

--- src/main/java/com/onlineBanking/card/util/ConstantUtil.java
+++ src/main/java/com/onlineBanking/card/util/ConstantUtil.java
@@ -3,5 +3,5 @@ package com.onlineBanking.card.util;
 public class ConstantUtil {
 	public static final String CARD_NOT_FOUND = "Card not found";
 	public static final String CARD_CREATED = "Card deactivated successfully";
-	public static final String METADATA_SERVICE_URL ="http://localhost:8083/api/v1/accountType/" ;
+	public static final String METADATA_SERVICE_URL ="http://localhost:8084/api/v1/cardType/" ;
 }
