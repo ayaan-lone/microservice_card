47b30c6191c6275919774b142e966f21c77b5b30
Ayaan Lone <ayaan.lone@xebia.com>
2024-07-25 02:54:53 +0530

    Dupli DB Card, SetCardBalance Fixed

--- src/main/java/com/onlineBanking/card/service/CardService.java
+++ src/main/java/com/onlineBanking/card/service/CardService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import com.onlineBanking.card.entity.Card;
 import com.onlineBanking.card.entity.CardType;
-import com.onlineBanking.card.entity.TransactionType;
 import com.onlineBanking.card.exception.CardApplicationException;
+import com.onlineBanking.card.request.CardTransactionRequestDto;
 import com.onlineBanking.card.request.CreateCardRequestDto;
 
 public interface CardService {
@@ -20,6 +20,6 @@ public interface CardService {
 	String handleTransaction(long userId, long cardNumber, long amount) throws CardApplicationException;
 	CardType fetchCardType(long userId, long cardNumber) throws CardApplicationException;
 	double fetchCardBalance(long userId, long cardNumber) throws CardApplicationException;
-	String updateBalance(Long userId, Long cardNumber, Double amount, TransactionType transactionType) throws CardApplicationException;
+	String updateBalance(CardTransactionRequestDto cardTransactionRequestDto) throws CardApplicationException;
 
 }
